5fc6040b75abf6f01aab2d495660b496b094df07
mihishkaasinha <72974069+mihishkaasinha@users.noreply.github.com>
2020-11-14 12:19:08 +0530

    Add files via upload

---
+++ newton_ball.js
@@ -0,0 +1,30 @@
+class Bob
+{
+  constructor(x, y, r)
+  {
+    var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':1.0
+    }
+
+    this.body = Bodies.circle(x, y, r, options);
+    this.r = r;
+    World.add(world, this.body);
+
+  }
+
+display(){
+        
+    push ();
+    translate(this.body.position.x, this.body.position.y);
+    rotate(this.body.angle);
+    ellipseMode(RADIUS)
+    ellipse(0, 0, this.r, this.r) 
+    pop();
+}
+
+}
+
+
+
